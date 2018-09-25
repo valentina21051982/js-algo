af5c755bec8a594706cc380921629ea534da1390
valentina <vgadajhto@gmail.com>
2018-09-25 21:55:02 +0300

    Changes for next implementation of algorithms

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-function writeAlgoDemo() {
+function writeAlgoDemo1() {
     var side_a = Math.random() * 10 + 5;
     var side_b = Math.random() * 10 + 5;
     document.write("<p>Side a=" + side_a + "Side b=" + side_b + "</p>");
     document.write("<p>Обьем куба" + side_a * side_a * side_a + "</p>");
     document.write("<p>Площадь квадрата" + side_a * side_a + "</p>");
     document.write("<p>Площадь прямоугольника" + side_a * side_b + "</p>");
+}
+
+function writeAlgoDemo2() {
+    var side_a = Math.random() * 10 + 5;
+    var side_b = Math.random() * 10 + 5;
+    document.write("<p>Side a=" + side_a + "Side b=" + side_b + "</p>");
+    document.write("<p>Площадь прямоугольного треугольника" + side_a * side_b / 2 + "</p>");
 }
