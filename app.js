bedf449613701e59c8a26a7a8cf8382a161ebb4c
valentina <vgadajhto@gmail.com>
2018-09-25 21:38:18 +0300

    Changes(view) in writeAlgoDemo”

---
+++ app.js
@@ -0,0 +1,8 @@
+function writeAlgoDemo() {
+    var side_a = Math.random() * 10 + 5;
+    var side_b = Math.random() * 10 + 5;
+    document.write("<p>Side a=" + side_a + "Side b=" + side_b + "</p>");
+    document.write("<p>Обьем куба" + side_a * side_a * side_a + "</p>");
+    document.write("<p>Площадь квадрата" + side_a * side_a + "</p>");
+    document.write("<p>Площадь прямоугольника" + side_a * side_b + "</p>");
+}
